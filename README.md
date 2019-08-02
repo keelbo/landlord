c672ce3e83be488ac3ab17449c96cf59040314da
Keelbo <sachin.aaglave>
2019-08-02 13:43:40 +0530

    Landlord basic implementation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # landlord
-# landlord
+
+# Build command
+
+g++ lordApp.cpp landlord.cpp -o a.out
+
+# Run executable
+
+./a.out
---
+++ lordApp.cpp
@@ -0,0 +1,9 @@
+// client file for apart program
+#include "landlord.h"
+int main()
+{
+    userInterface theUserInterface;
+    theUserInterface.interact();
+    return 0;
+}
+////////////////////////end file lordApp.cpp////////////////////
